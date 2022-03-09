24a7ca703501737a0ed6de9567ba15c4bb7629d0
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-09 13:38:35 -0300

    feat: add body background

--- src/components/public/public-template.tsx
+++ src/components/public/public-template.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next'
-import styles from '@/styles/Global.module.scss'
 import Header from '@/components/public/header/header'
-import Head from 'next/head'
-
 
 const PublicTemplate: NextPage = ({children}): JSX.Element => {
   return (
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,18 @@
 import { ProtectedLayout } from "@/components/ProtectLayout"
+import { getUserLocalStore } from "@/resources/contexts/Auth/utils"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function Dashboard ()  {
+    const router = useRouter()
+    
+    useEffect(() => {
+        const user = getUserLocalStore()
+
+        if(!user) {
+            router.push('./login')
+        } 
+    })
     return(
         <ProtectedLayout>
             <h1>Profile Access </h1>
