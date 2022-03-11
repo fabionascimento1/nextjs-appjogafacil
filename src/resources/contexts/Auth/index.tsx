59b3fecc2ed17dc5df756fa8e01ac8b69f37df4f
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-11 12:53:10 -0300

    feat: redirect when logged success

--- src/resources/contexts/Auth/index.tsx
+++ src/resources/contexts/Auth/index.tsx
@@ -2,13 +2,14 @@ import { Api } from '@/services/api-node'
 import { useRouter } from 'next/router'
 import { createContext, useEffect, useState } from 'react'
 import {IUser, IAuthContext, IAuthProvider} from './types'
-import { getUserLocalStore, LoginRequest, setUserLocalStorage } from './utils'
+import { getUserLocalStore, setUserLocalStorage } from './utils'
 
 export const AuthContext = createContext<IAuthContext>({} as IAuthContext)
 
 export const AuthProvider = ({ children }: IAuthProvider ) => {
   const [user, setUser] = useState<IUser | null>()
   const [mainError, setMainError] = useState()
+  const router = useRouter()
 
   useEffect(() => {
     const user = getUserLocalStore()
@@ -24,6 +25,7 @@ export const AuthProvider = ({ children }: IAuthProvider ) => {
         const payload = {token: response.data.token, email}
         setUser(payload)
         setUserLocalStorage(payload)
+        router.push('/dashboard')
       })
       .catch((error) => { 
         setMainError(error.response.data["error"])
