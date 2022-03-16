452eae7ffc81c7c0967d1912c6302bf1233f7684
Fabio Nascimento <agsuperpoder@gmail.com>
2022-03-16 11:31:06 -0300

    test: PageLogin start render with initial state

--- __tests__/pages/login/login.spec.tsx
+++ __tests__/pages/login/login.spec.tsx
@@ -1,12 +1,30 @@
-import { render, screen } from '@testing-library/react'
-import Login from "@/pages/login"
+import { render, screen, cleanup, RenderResult } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import LoginComponent from "@/pages/login"
+
+type SutTypes = {
+  view: RenderResult
+}
+const makeSut = (): SutTypes => {
+    const view = render(<LoginComponent />)
+    return {
+      view
+    }
+}
 
 describe('Login', () => {
+  afterEach(cleanup)
+
   test('render email input', () => {
-    render(<Login />);
- 
+    const { view } = makeSut()  
     const inputEl = screen.getByTestId("email-input");
     expect(inputEl).toBeInTheDocument();
     expect(inputEl).toHaveAttribute("type", "email");
+  });
+
+  test('Should start render with initial state', () => {
+     const { view } = makeSut()  
+    const submitButton = screen.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   });
 })
